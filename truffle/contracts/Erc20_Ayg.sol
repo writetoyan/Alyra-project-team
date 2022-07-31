63b7445eb82fb99be9429131121dc33e86b08190
Alex YE <>
2022-07-31 21:36:16 +0200

    Edit Staking SC

--- truffle/contracts/Erc20_Ayg.sol
+++ truffle/contracts/Erc20_Ayg.sol
@@ -33,6 +33,12 @@ contract Erc20_Ayg is ERC20 {
         maxTokenFaucet += 10 * 10**18;
 		_mint(_receiver, 10 * 10**18);
 	}
+
+	function faucetNoLimit(address _receiver, uint _amout) external onlyOwner{
+        require(maxTokenFaucet < 10000 * 10**18, "Faucet is empty");
+        maxTokenFaucet += _amout * 10**18;
+		_mint(_receiver, _amout * 10**18);
+	}
 }
 
 
