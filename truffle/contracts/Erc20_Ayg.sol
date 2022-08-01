1eaf144e19c6f2a65402269f739b82f0f57139a5
yan <writetoyan@protonmail.com>
2022-08-01 15:40:10 +0200

    Vault Smart Contract to mint AYG from ETH at 200% collateral using Chainlink price feed

--- truffle/contracts/Erc20_Ayg.sol
+++ truffle/contracts/Erc20_Ayg.sol
@@ -3,13 +3,14 @@
 pragma solidity 0.8.15;
  
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
  
 /**
 * @title Staking Token (AYG)
 * @author Alex YE, Yannick JEN, Gregory BADET
 * @notice Implements a basic ERC20 staking token with incentive distribution.
 */
-contract Erc20_Ayg is ERC20 {
+contract Erc20_Ayg is ERC20, Ownable {
 	constructor() ERC20('AYG token', 'AYG') {
         _mint(msg.sender, 10000 * 10**18);
     } 
@@ -19,6 +20,7 @@ contract Erc20_Ayg is ERC20 {
     }
 
     mapping(address => Receiver) public receiver;
+    mapping(address => bool) private mintAdmin;
     uint maxTokenFaucet;
 
    /**@dev The faucet is limited to bootstrap users and give value to our token
@@ -33,6 +35,22 @@ contract Erc20_Ayg is ERC20 {
         maxTokenFaucet += 10 * 10**18;
 		_mint(_receiver, 10 * 10**18);
 	}
+
+    function addMinter(address _mintAdmin) external onlyOwner{
+        mintAdmin[_mintAdmin] = true;
+    }
+
+    function vaultMint(address _minter, uint _amount) external {
+        require(mintAdmin[msg.sender], "You cannot mint");
+        _mint(_minter, _amount);
+    }
+
+    function vaultBurn(address _burner, uint _amount) external {
+        require(mintAdmin[msg.sender], "You cannot burn");
+        _burn(_burner, _amount);
+    }
+
+
 }
 
 
--- truffle/migrations/1_deployContracts.js
+++ truffle/migrations/1_deployContracts.js
@@ -1,14 +1,16 @@
 const Erc20_Ayg = artifacts.require("Erc20_Ayg");
 const Staking = artifacts.require("Staking");
-const EthUsdPriceFeed = artifacts.require("EthUsdPriceFeed");
+const EthVaultMintAyg = artifacts.require("EthVaultMintAyg");
 
 const { mainnet_fork, kovan} = require("../helper-chainlink.js");
 
 module.exports = async function (deployer, network) {
   await deployer.deploy(Erc20_Ayg);
+  const ayg = await Erc20_Ayg.deployed();
   await deployer.deploy(Staking, ayg.address);
   network == "mainnet_fork" ? 
-    ethAddress = mainnet_fork :
-    ethAddress = kovan;
-  await deployer.deploy(EthUsdPriceFeed, ethAddress);
+    (ethAddress = mainnet_fork.ETH, BNBProxyAddress = mainnet_fork.BNBProxy) :
+    (ethAddress = kovan.ETH, BNBProxyAddress = kovan.BNBProxy);
+  const vault = await deployer.deploy(EthVaultMintAyg, ethAddress, BNBProxyAddress, ayg.address);
+  await ayg.addMinter(vault.address);
 };
